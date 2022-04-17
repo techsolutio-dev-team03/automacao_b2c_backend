292d3476118010e53692d3728e89be472cf97cb7
automacao <automacao@172.16.192.62>
2022-04-17 01:03:37 -0300

    caderno funcional teste 69 troca de senha ASK ECNT

--- HGUmodels/models/probe_AskeyECNT/functionalProbe.py
+++ HGUmodels/models/probe_AskeyECNT/functionalProbe.py
@@ -57,3 +57,83 @@ class HGU_AskeyECNT_functionalProbe(HGU_AskeyECNT):
             self._dict_result.update({"obs": str(exception)})
         finally:
             return self._dict_result
+
+    
+    def changeAdminPassword_69(self, flask_username, new_password):
+
+        def open_change_password(driver):
+            driver.switch_to.default_content()
+            time.sleep(5)
+            #driver.switch_to.frame("menufrm")
+            driver.find_element_by_xpath('//*[@id="accordion"]/li[3]/a').click()
+            time.sleep(1)
+            link = driver.find_element_by_xpath('//*[@id="accordion"]/li[3]/ul/li[2]/a').click()
+            time.sleep(1)
+
+        def admin_authentication(driver, user, old_password):
+            driver.switch_to.default_content()
+            time.sleep(1)
+            #driver.switch_to.frame("basefrm")
+            user_input = driver.find_element_by_xpath('//*[@id="txtUser"]')
+            user_input.send_keys(user)
+            pass_input = driver.find_element_by_xpath('//*[@id="txtPass"]')
+            pass_input.send_keys(old_password)
+            login_button = driver.find_element_by_xpath('//*[@id="btnLogin"]')
+            time.sleep(1)
+            login_button.click()
+            time.sleep(1)
+
+        def changing_password(driver, old_password, new_password):
+            driver.switch_to.default_content()
+            time.sleep(1)
+            #driver.switch_to.frame("basefrm")
+            time.sleep(1)
+            gerenc_senha_old_valor = driver.find_element_by_xpath('//*[@id="txtOldPass"]').send_keys(str(old_password))
+            time.sleep(1)
+            gerenc_senha_new_valor = driver.find_element_by_xpath('//*[@id="txtNewPass"]').send_keys(str(new_password))
+            time.sleep(1)
+            gerenc_senha_new_valor2 = driver.find_element_by_xpath('//*[@id="txtConfirm"]').send_keys(str(new_password))
+            time.sleep(1)
+            config_wifi5_basico_ssid_senha_salvar = driver.find_element_by_xpath('//*[@id="btnSave"]').click()  ### SAVE BUTTON
+            time.sleep(8)  ### Tempo para recarregar a página após salvar as configs
+
+        def change_password_back(driver, user, old_password, new_password):
+            admin_authentication(driver, user, new_password)
+            changing_password(driver, new_password, old_password)
+
+        self._driver.execute_script("window.alert = function() {};")
+        
+        print('\n\n == Abrindo URL == ')
+        self._driver.get('http://' + self._address_ip + '/')
+
+
+        if re.match(r"^.*(?=.{8,})(?=.*\d)(?=.*[a-z]).*$", new_password):
+            print('SenhaAdmin de Entrada cumpre requisitos...')
+            
+            try:
+                print(' == Solicitando troca de senha == ')
+                open_change_password(self._driver)
+
+                ########################################################################################
+                print(' == Autenticando == ')
+                admin_authentication(self._driver, self._username, self._password)
+                time.sleep(5)
+
+
+                #########################################################################################
+                print(' == Troca de senha == ')
+                changing_password(self._driver, self._password, new_password)
+
+
+                change_password_back(self._driver, self._username, self._password, new_password)
+
+                self._dict_result.update({"Resultado_Probe": "OK",'result':'passed', "obs": None})
+
+            except NoSuchElementException as exception:
+                print(exception)
+                self._dict_result.update({"obs": str(exception)})
+
+            finally:
+                print(' == Fim do teste == ')
+                return self._dict_result
+
